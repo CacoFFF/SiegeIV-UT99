52617bf206a9fd539392b6d9fd16c6a2f33ba10c
Fernando Velázquez <caco_fff@hotmail.com>
2020-10-27 02:49:40 -0400

    SiegeNative - setup VS 2019 project.

--- SiegeNative/Inc/SiegeNative.h
+++ SiegeNative/Inc/SiegeNative.h
@@ -15,20 +15,12 @@
 //#define XC_CORE_API DLL_IMPORT
 
 #ifndef __LINUX_X86__
-	#define CORE_API DLL_IMPORT
 	#define SIEGENATIVE_API DLL_EXPORT
 	#define _WIN32_WINNT 0x0501
-	#include <windows.h>
-	#include <shlobj.h>
 #else
 	//If we don't disable this, v436 std (some builds) won't be able to run this mod
-	#define DO_GUARD 0
 	#include <unistd.h>	
 #endif
-
-//Safely empty a dynamic array on UT v451
-#define SafeEmpty( A) if (A.GetData()) A.Empty()
-#define SafeEmptyR( A) if (A->GetData()) A->Empty()
 
 
 //Engine includes
@@ -36,21 +28,9 @@
 
 
 //Script
-#define warnf GWarn->Logf
-#define debugf GLog->Logf
 #define NAME_SiegeNative (EName)SIEGENATIVE_SiegeNative.GetIndex()
-
-//XC_Core
-//#include "UnXC_Math.h"
-//#include "MEMCPY_AMD.h" //Should be better than the default appMemcpy
 
 //Classes here
 #include "SiegeNativeClasses.h"
-
-#ifdef __LINUX_X86__
-	#undef CPP_PROPERTY
-	#define CPP_PROPERTY(name) \
-		EC_CppProperty, (BYTE*)&((ThisClass*)1)->name - (BYTE*)1
-#endif
 
 #endif
